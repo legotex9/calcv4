d843b0f5e618664dae28ac966a36d19cc44046d3
legotex9 <35179275+legotex9@users.noreply.github.com>
2018-04-06 15:42:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,73 +2,130 @@ from termcolor import colored
 import time
 q = 0
 x = float(.25/2)
-print (colored('                            ____________','cyan'))
-time.sleep(x/2)
-print (colored('                            \|M  A  I  N|\ ','cyan'))
-time.sleep(x/2)
-print (colored('                             \|-=-=-=-=-=|\ ','cyan'))
-time.sleep(x/2)
-print (colored('                              \|M  E  N  U|\ ','cyan'))
-time.sleep(x/2)
-print (colored('|-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=|', 'cyan'))
-time.sleep(x)
-print (colored('|   AREA   |||||||| PERIMITER |||||||| SURFACE AREA  ||||||||    VOLUME   ||||||||', 'cyan'))
-time.sleep(x)
-print (colored('|-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=|', 'cyan'))
-time.sleep(x)
-print (colored('|  SQUARE  | 1.01 |   SQUARE  | 2.01 |      CUBE     | 3.01 |     CUBE    | 4.01 |', 'cyan'))
-time.sleep(x)
-print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
-time.sleep(x)
-print (colored('| TRIANGLE | 1.02 |  TRIANGLE | 2.02 |  RECTANGULAR  | 3.02 | RECTANGULAR | 4.02 |', 'cyan'))
-time.sleep(x)
-print (colored('|          |      |           |      |     PRISM     |      |    PRISM    |      |', 'cyan'))
-time.sleep(x)
-print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
-time.sleep(x)
-print (colored('|  CIRCLE  | 1.03 |   CIRCLE  | 2.03 |   TRIANGULAR  | 3.03 |  TRIANGULAR | 4.03 |', 'cyan'))
-time.sleep(x)
-print (colored('|          |      |           |      |     PRISM     |      |    PRISM    |      |', 'cyan'))
-time.sleep(x)
-print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
-time.sleep(x)
-print (colored('|    TBA   | 1.04 |    TBA    | 2.04 |      CONE     | 3.04 |     CONE    | 4.04 |', 'cyan'))
-time.sleep(x)
-print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
-time.sleep(x)
-print (colored('|    TBA   | 1.05 |    TBA    | 2.05 |  RECTANGULAR  | 3.05 | RECTANGULAR | 4.05 |', 'cyan'))
-time.sleep(x)
-print (colored('|          |      |           |      |    PYRAMID    |      |  PYRAMID    |      |', 'cyan'))
-time.sleep(x)
-print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
-time.sleep(x)
-print (colored('|    TBA   | 1.06 |    TBA    | 2.06 |   TRIANGULAR  | 3.06 |  TRIANGULAR | 4.06 |', 'cyan'))
-time.sleep(x)
-print (colored('|          |      |           |      |    PYRAMID    |      |   PYRAMID   |      |', 'cyan'))
-time.sleep(x)
-print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
-time.sleep(x)
-print (colored('|    TBA   | 1.07 |    TBA    | 2.07 |     SPHERE    | 3.07 |    SPHERE   | 4.07 |', 'cyan'))
-time.sleep(x)
-print (colored('|-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=|', 'cyan'))
-time.sleep(x)
-print (colored('|      OTHER      ||||||||','cyan'))
-time.sleep(x)
-print (colored('|-=-=-=-=-=-=-=-=-=-=-=-=|','cyan'))
-time.sleep(x)
-print (colored('|     VARIBLES    | 0.01 |','cyan'))
-time.sleep(x)
-print (colored('|   TRANSLATION   |      |','cyan'))
-time.sleep(x)
-print (colored('|++++++++++++++++++++++++|','cyan'))
-time.sleep(x)
-print (colored('| EQUATIONS  USED | 0.02 |','cyan'))
-time.sleep(x)
-print (colored('|++++++++++++++++++++++++|','cyan'))
-time.sleep(x)
-print (colored('|      EXIT       | 0.03 |','cyan'))
-time.sleep(x)
-print (colored('|-=-=-=-=-=-=-=-=-=-=-=-=|','cyan'))
+def mainMenuNorm():
+	print (colored('                            ____________','cyan'))
+	time.sleep(x/2)
+	print (colored('                            \|M  A  I  N|\ ','cyan'))
+	time.sleep(x/2)
+	print (colored('                             \|-=-=-=-=-=|\ ','cyan'))
+	time.sleep(x/2)
+	print (colored('                              \|M  E  N  U|\ ','cyan'))
+	time.sleep(x/2)
+	print (colored('|-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=|', 'cyan'))
+	time.sleep(x)
+	print (colored('|   AREA   |||||||| PERIMITER |||||||| SURFACE AREA  ||||||||    VOLUME   ||||||||', 'cyan'))
+	time.sleep(x)
+	print (colored('|-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=|', 'cyan'))
+	time.sleep(x)
+	print (colored('|  SQUARE  | 1.01 |   SQUARE  | 2.01 |      CUBE     | 3.01 |     CUBE    | 4.01 |', 'cyan'))
+	time.sleep(x)
+	print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
+	time.sleep(x)
+	print (colored('| TRIANGLE | 1.02 |  TRIANGLE | 2.02 |  RECTANGULAR  | 3.02 | RECTANGULAR | 4.02 |', 'cyan'))
+	time.sleep(x)
+	print (colored('|          |      |           |      |     PRISM     |      |    PRISM    |      |', 'cyan'))
+	time.sleep(x)
+	print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
+	time.sleep(x)
+	print (colored('|  CIRCLE  | 1.03 |   CIRCLE  | 2.03 |   TRIANGULAR  | 3.03 |  TRIANGULAR | 4.03 |', 'cyan'))
+	time.sleep(x)
+	print (colored('|          |      |           |      |     PRISM     |      |    PRISM    |      |', 'cyan'))
+	time.sleep(x)
+	print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
+	time.sleep(x)
+	print (colored('|    TBA   | 1.04 |    TBA    | 2.04 |      CONE     | 3.04 |     CONE    | 4.04 |', 'cyan'))
+	time.sleep(x)
+	print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
+	time.sleep(x)
+	print (colored('|    TBA   | 1.05 |    TBA    | 2.05 |  RECTANGULAR  | 3.05 | RECTANGULAR | 4.05 |', 'cyan'))
+	time.sleep(x)
+	print (colored('|          |      |           |      |    PYRAMID    |      |  PYRAMID    |      |', 'cyan'))
+	time.sleep(x)
+	print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
+	time.sleep(x)
+	print (colored('|    TBA   | 1.06 |    TBA    | 2.06 |   TRIANGULAR  | 3.06 |  TRIANGULAR | 4.06 |', 'cyan'))
+	time.sleep(x)
+	print (colored('|          |      |           |      |    PYRAMID    |      |   PYRAMID   |      |', 'cyan'))
+	time.sleep(x)
+	print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
+	time.sleep(x)
+	print (colored('|    TBA   | 1.07 |    TBA    | 2.07 |     SPHERE    | 3.07 |    SPHERE   | 4.07 |', 'cyan'))
+	time.sleep(x)
+	print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
+	time.sleep(x)
+	print (colored('|    TBA   | 1.08 |    TBA    | 2.08 |    CYLINDER   | 3.08 |   CYLINDER  | 4.08 |', 'cyan'))
+	time.sleep(x)
+	print (colored('|-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=|', 'cyan'))
+	time.sleep(x)
+	print (colored('|      OTHER      ||||||||','cyan'))
+	time.sleep(x)
+	print (colored('|-=-=-=-=-=-=-=-=-=-=-=-=|','cyan'))
+	time.sleep(x)
+	print (colored('|     VARIBLES    | 0.01 |','cyan'))
+	time.sleep(x)
+	print (colored('|   TRANSLATION   |      |','cyan'))
+	time.sleep(x)
+	print (colored('|++++++++++++++++++++++++|','cyan'))
+	time.sleep(x)
+	print (colored('| EQUATIONS  USED | 0.02 |','cyan'))
+	time.sleep(x)
+	print (colored('|++++++++++++++++++++++++|','cyan'))
+	time.sleep(x)
+	print (colored('|      EXIT       | 0.03 |','cyan'))
+	time.sleep(x)
+	print (colored('|++++++++++++++++++++++++|','cyan'))
+	time.sleep(x)
+	print (colored('|     SHOW MENU   | 0.04 |','cyan'))
+	time.sleep(x)
+	print (colored('|++++++++++++++++++++++++|','cyan'))
+	time.sleep(x)
+	print (colored('|     SETTINGS    | 0.05 |','cyan'))
+	time.sleep(x)
+	print (colored('|-=-=-=-=-=-=-=-=-=-=-=-=|','cyan'))
+	time.sleep(x)
+def mainMenuNoSleep():
+	print('wip')
+	print (colored('                            ____________','cyan'))
+	print (colored('                            \|M  A  I  N|\ ','cyan'))
+	print (colored('                             \|-=-=-=-=-=|\ ','cyan'))
+	print (colored('                              \|M  E  N  U|\ ','cyan'))
+	print (colored('|-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=|', 'cyan'))
+	print (colored('|   AREA   |||||||| PERIMITER |||||||| SURFACE AREA  ||||||||    VOLUME   ||||||||', 'cyan'))
+	print (colored('|-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=|', 'cyan'))
+	print (colored('|  SQUARE  | 1.01 |   SQUARE  | 2.01 |      CUBE     | 3.01 |     CUBE    | 4.01 |', 'cyan'))
+	print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
+	print (colored('| TRIANGLE | 1.02 |  TRIANGLE | 2.02 |  RECTANGULAR  | 3.02 | RECTANGULAR | 4.02 |', 'cyan'))
+	print (colored('|          |      |           |      |     PRISM     |      |    PRISM    |      |', 'cyan'))
+	print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
+	print (colored('|  CIRCLE  | 1.03 |   CIRCLE  | 2.03 |   TRIANGULAR  | 3.03 |  TRIANGULAR | 4.03 |', 'cyan'))
+	print (colored('|          |      |           |      |     PRISM     |      |    PRISM    |      |', 'cyan'))
+	print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
+	print (colored('|    TBA   | 1.04 |    TBA    | 2.04 |      CONE     | 3.04 |     CONE    | 4.04 |', 'cyan'))
+	print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
+	print (colored('|    TBA   | 1.05 |    TBA    | 2.05 |  RECTANGULAR  | 3.05 | RECTANGULAR | 4.05 |', 'cyan'))
+	print (colored('|          |      |           |      |    PYRAMID    |      |  PYRAMID    |      |', 'cyan'))
+	print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
+	print (colored('|    TBA   | 1.06 |    TBA    | 2.06 |   TRIANGULAR  | 3.06 |  TRIANGULAR | 4.06 |', 'cyan'))
+	print (colored('|          |      |           |      |    PYRAMID    |      |   PYRAMID   |      |', 'cyan'))
+	print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
+	print (colored('|    TBA   | 1.07 |    TBA    | 2.07 |     SPHERE    | 3.07 |    SPHERE   | 4.07 |', 'cyan'))
+	print (colored('|++++++++++|++++++|+++++++++++|++++++|+++++++++++++++|++++++|+++++++++++++|++++++|', 'cyan'))
+	print (colored('|    TBA   | 1.08 |    TBA    | 2.08 |    CYLINDER   | 3.08 |   CYLINDER  | 4.08 |', 'cyan'))
+	print (colored('|-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=|', 'cyan'))
+	print (colored('|      OTHER      ||||||||||     OTHER EQUATIONS     ||||||||','cyan'))
+	print (colored('|-=-=-=-=-=-=-=-=-=-=-=-=|||=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=|','cyan'))
+	print (colored('|     VARIBLES    | 0.01 |||    PYTHAGOREAN THEROM   | 5.01 |','cyan'))
+	print (colored('|   TRANSLATION   |      |||++++++++++++++++++++++++++++++++|','cyan'))
+	print (colored('|++++++++++++++++++++++++|||           TBA           | 5.02 |','cyan'))
+	print (colored('| EQUATIONS  USED | 0.02 |||++++++++++++++++++++++++++++++++|','cyan'))
+	print (colored('|++++++++++++++++++++++++|||           TBA           | 5.03 |','cyan'))
+	print (colored('|       EXIT      | 0.03 |||++++++++++++++++++++++++++++++++|','cyan'))
+	print (colored('|++++++++++++++++++++++++|||           TBA           | 5.04 |','cyan'))
+	print (colored('|     SHOW MENU   | 0.04 |||++++++++++++++++++++++++++++++++|','cyan'))
+	print (colored('|++++++++++++++++++++++++|||           TBA           | 5.05 |','cyan'))
+	print (colored('|     SETTINGS    | 0.05 |||=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-|','cyan'))
+	print (colored('|-=-=-=-=-=-=-=-=-=-=-=-=|||','cyan'))
+mainMenuNorm()
 while q<1:
 	pi = float(3.14159)
 	x = float(x/2)
@@ -159,7 +216,26 @@ while q<1:
 	elif a == 3.03:
 		print('wip')
 		time.sleep(x1)
-	
+		
+	#surface area of sphere
+	elif a == 3.07:
+		print(colored('sa = 4 * (pi * r ^2)','yellow'))
+		r = float(input('r = '))
+		sa = 4*(pi*r**2)
+		sa = str(sa)
+		print(colored('sa = ' + sa,'green'))
+		time.sleep(x1)
+		
+	#surfeace area of cylinder
+	elif a == 3.08:
+		print(colored('sa = (2 * (pi * r) * h) + 2 * (pi * r ^ 2)'))
+		r = float(input('r ='))
+		h = float(input('h ='))
+		sa = (2*(pi*r)*h)+2*(pi*r**2)
+		sa = str(sa)
+		print (colored('sa = ' + sa,'green'))
+		time.sleep(x1)
+		
  #	------------------------------------------------
 	
 	#volume of cube
@@ -189,6 +265,49 @@ while q<1:
 		print('wip')
 		time.sleep(x1)
 	
+	#volume of cone
+	elif a == 4.04:
+		print(colored('v = ((pi * r ^ 2) * h) /3','yellow'))
+		r = float(input('r = '))
+		h = float(input('h = '))
+		v = ((pi * r**2)*h)/3
+		v = str(v)
+		print(colored('v = ' + v,'green'))
+		
+	#volume of rectangular pyramid
+	elif a == 4.05:
+		print(colored('v = ((l * w) * h) /3','yellow'))
+		l = float(input('l = '))
+		w = float(input('w = '))
+		h = float(input('h = '))
+		v = ((l*w)*h)/3 
+		v = str(v)
+		print(colored('v = ' + v,'green'))
+		time.sleep(x1)
+	
+	#volume of triangular pyramid
+	elif a == 4.06:
+		print('wip')
+		time.sleep(x1)
+	
+	#volume of sphere
+	elif a == 4.07:
+		print(colored('v = ((pi * r ^ 3) * 4) /3'))
+		r = float(input('r = '))
+		v = ((pi*r**3)*4)/3
+		v = str(v)
+		print(colored('v = ' + v,'green'))
+		
+	#volume of cylinder
+	elif a == 4.08:
+		print(colored(' (pi * r ^2) * h','yellow'))
+		r = float(input('r = '))
+		h = float(input('h = '))
+		v = (pi*r**2)*h
+		v = str(v)
+		print(colored('v = ' + v,'green'))
+		time.sleep(x1)
+		
  #	------------------------------------------------
 	
 	#varible translation
@@ -207,6 +326,9 @@ while q<1:
 		print(colored('p = (2 * l) + (2 * w)', 'yellow'))
 		time.sleep(x1)
 	
+ #	------------------------------------------------
+	
+	#exit
 	elif a == 0.03:
 		print(colored('shuting','red'))
 		time.sleep(1)
@@ -216,6 +338,21 @@ while q<1:
 	
  #	------------------------------------------------
  
+ #show main menu
+	elif a == 0.04:
+ 		mainMenuNorm()
+ 		time.sleep(x1)
+ 		
+ #	------------------------------------------------
+ 
+	#settings
+	elif a == 0.05:
+ 		print('tba')
+ 		time.sleep(x1)
+ 		
+ #	------------------------------------------------
+ 
+	#invalid input
 	else:
 		print (colored("that input is not listed or incorect. please try again.","red"))
 		time.sleep(x1)
